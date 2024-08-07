1075e31e338db80c61935f96c08946bc955fca02
张长悦 <dancingcodes@qq.com>
2024-08-07 14:13:50 +0800

    1

--- src/api/music/index.ts
+++ src/api/music/index.ts
@@ -1,3 +1,15 @@
 import request from "@/utils/request";
 
-export const searchMusic = params => request.post('/music/searchMusic', params)
+interface ISearchMusicParams {
+    name: string;
+    offset?: number;
+    limit?: number;
+}
+
+export const searchMusic = (params: ISearchMusicParams) => request.get('/music/searchMusic', { params })
+
+
+interface ISaveMusicParams {
+    id: string;
+}
+export const saveMusic = (params: ISaveMusicParams) => request.get('/music/saveMusic', { params })
