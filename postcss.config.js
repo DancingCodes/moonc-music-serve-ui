75eacbb79186182451b2db3d5e1e1734cfe33d94
张长悦 <dancingcodes@qq.com>
2024-08-08 11:10:53 +0800

    1

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,8 @@ import postcssPxToViewport from 'postcss-px-to-viewport-8-plugin';
 export default {
     plugins: [
         postcssPxToViewport({
-            viewportWidth: 1920, // 视口宽度，可以根据设计稿调整
-            viewportHeight: 1080, // 视口高度，可以根据设计稿调整
+            viewportWidth: 1920,
+            viewportHeight: 1080,
         })
     ]
 }
