5e5fc5dfcc87008ae28ea54a4ce5b97d304ffa9f
张长悦 <dancingcodes@qq.com>
2024-08-09 12:22:35 +0800

    1

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,7 +9,11 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     IEpSearch: typeof import('~icons/ep/search')['default']
     IEpUploadFilled: typeof import('~icons/ep/upload-filled')['default']
+  }
+  export interface ComponentCustomProperties {
+    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
